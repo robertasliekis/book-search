3c0817ce3efeddf861166656902cdb7275fbf67c
Robertas Liekis <robertasliekis@gmail.com>
2020-08-07 13:24:48 +0300

    Updates

---
+++ precache-manifest.242ec52abffcdd629d4a7ba4ed4ed84a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d4f28549f45e4af6c9e160ae46ad89",
+    "url": "/book-search/index.html"
+  },
+  {
+    "revision": "28140b391fb37f4719d8",
+    "url": "/book-search/static/css/main.0fae755c.chunk.css"
+  },
+  {
+    "revision": "8556e4bda053fb5560c0",
+    "url": "/book-search/static/js/2.b3ec86e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-search/static/js/2.b3ec86e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28140b391fb37f4719d8",
+    "url": "/book-search/static/js/main.afba36b7.chunk.js"
+  },
+  {
+    "revision": "1692f6378f07580b4ced",
+    "url": "/book-search/static/js/runtime-main.83c1ef20.js"
+  }
+]);
